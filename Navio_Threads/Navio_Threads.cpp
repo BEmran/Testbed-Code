55ce8868973b61f9c76fa3df2317deddc93c639a
Bara Emran <bara.emran@gmail.com>
2017-08-25 13:05:02 -0700

    add pwm functions

--- Navio_Threads/Navio_Threads.cpp
+++ Navio_Threads/Navio_Threads.cpp
@@ -7,6 +7,14 @@
 #include "Navio_Threads.hpp"
 #include "SamplingTime.h"    // sampling time
 
+#include <stdio.h>
+//#include <sys/socket.h>
+//#include <stdlib.h>
+//#include <netinet/in.h>
+//#include <string.h>         // strlen
+//#include <arpa/inet.h>      //inet_pton
+#define PORT 8080
+
 int main(int argc, char** argv) {
     // Welcome msg
     printf("Start Program\n"); 
@@ -55,14 +63,26 @@ void *conThread(void *data)
 	my_data = (struct dataStruct *) data;
         SamplingTime st(400);
         float dt, dtsumm = 0;
+        // Initialize PWM
+	setAllPWM(my_data->PWMval, _MinSpeed);
+	my_data->pwm = new PWM;
+
+	// Initialize PWM
+	if (!initPWM(my_data->pwm))
+	{
+		printf("Can not initialize PWM. Exit control thread\n");
+		pthread_exit(NULL);
+	}
+        
 	// Main loop
 	while (!_CloseRequested)
 	{   
             dt = st.tsCalculat();
+            sendPWM(my_data->pwm, my_data->PWMval);
             dtsumm += dt;
             if (dtsumm > 1){
                 dtsumm = 0;
-                printf("control thread with frequency %f\n", 1/dt);
+                printf("control thread with %d Hz\n", int(1/dt));
             }
 	}
 	// Exit procedure
@@ -87,7 +107,7 @@ void *senThread(void *data)
             dtsumm += dt;
             if (dtsumm > 1){
                 dtsumm = 0;
-                printf("sensor thread with frequency %f\n", 1/dt);
+                printf("sensor thread with %d Hz\n", int(1/dt));
             }
 	}
 	// Exit procedure
@@ -105,6 +125,31 @@ void *tcpThread(void *data)
 	my_data = (struct dataStruct *) data;
         SamplingTime  st(200);
         float dt, dtsumm = 0;
+//         // Network data
+//      
+//        struct sockaddr_in address;
+//        int sockfd = 0, valread;
+//        struct sockaddr_in serv_addr;
+//        char buffer[1024] = {0};
+//        if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+//        {
+//            printf("\n Socket creation error \n");
+//            //return -1;
+//        }
+//  
+//        memset(&serv_addr, '0', sizeof(serv_addr));
+//        serv_addr.sin_family = AF_INET;
+//        serv_addr.sin_port = htons(PORT);
+//      
+//        // Convert IPv4 and IPv6 addresses from text to binary form
+//        inet_pton(AF_INET, "192.1681.1.100", &serv_addr.sin_addr);
+//        if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+//        {
+//            printf("\nConnection Failed \n");
+//           // return -1;
+//        }
+//        
+    
 	// Main loop
 	while (!_CloseRequested)
 	{   
@@ -112,10 +157,57 @@ void *tcpThread(void *data)
             dtsumm += dt;
             if (dtsumm > 1){
                 dtsumm = 0;
-                printf("tcp thread with frequency %f\n", 1/dt);
+                printf("tcp thread with %d Hz\n", int(1/dt));
+                //sprintf(buffer, "%dHz\n", int(1 / dt));
+                //sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *) &serv_addr, sizeof (serv_addr));    
             }
 	}
 	// Exit procedure
 	printf("Exit tcp thread\n");
 	pthread_exit(NULL);
 }
+
+
+int initPWM(PWM *pwm)
+{
+	if (!pwm->init(_MOTOR1) && !pwm->init(_MOTOR2) && !pwm->init(_MOTOR3) && !pwm->init(_MOTOR4))
+	{
+		fprintf(stderr, "Output Enable not set. Are you root?\n");
+		return 0;
+	}
+
+	pwm->enable(_MOTOR1);
+	pwm->enable(_MOTOR1);
+	pwm->enable(_MOTOR1);
+	pwm->enable(_MOTOR1);
+
+	pwm->set_period(_MOTOR1, 400);
+	pwm->set_period(_MOTOR1, 400);
+	pwm->set_period(_MOTOR1, 400);
+	pwm->set_period(_MOTOR1, 400);
+
+	return 1;
+}
+
+// ********************************************************** //
+void setPWM(float PWMval[], float pwm1, float pwm2, float pwm3, float pwm4)
+{
+	PWMval[0] = pwm1;
+	PWMval[1] = pwm2;
+	PWMval[2] = pwm3;
+	PWMval[3] = pwm4;
+}
+// ********************************************************** //
+void setAllPWM(float PWMval[], float val)
+{
+	setPWM(PWMval, val, val, val, val);
+}
+// ********************************************************** //
+void sendPWM(PWM* pwm, float PWMval[])
+{
+	pwm->set_duty_cycle(_MOTOR1, PWMval[0]);
+	pwm->set_duty_cycle(_MOTOR2, PWMval[1]);
+	pwm->set_duty_cycle(_MOTOR3, PWMval[2]);
+	pwm->set_duty_cycle(_MOTOR4, PWMval[3]);
+}
+
--- Navio_Threads/include/Navio_Threads.hpp
+++ Navio_Threads/include/Navio_Threads.hpp
@@ -12,6 +12,14 @@
 #include <signal.h>     // signal ctrl+c
 #include <stdio.h>      // printf
 #include <unistd.h>     // usleep
+#include "../../Navio/PWM.h"    // PWM
+#include "../../Navio/Util.h"    // PWM
+#define _MOTOR1 0
+#define _MOTOR2 1
+#define _MOTOR3 2
+#define _MOTOR4 3
+#define _MinSpeed 1.0 // ms
+#define _MaxSpeed 2.0 // ms
 
 // ********************************************************** //
 // Global variables:
@@ -24,6 +32,8 @@ pthread_t _Thread_Tcp;
 // Define structures:
 struct dataStruct {
 	char buff[100];
+        float PWMval[4];
+        PWM *pwm;
 };
 
 // ********************************************************** //
@@ -32,5 +42,10 @@ void *conThread(void *data);
 void *senThread(void *data);
 void *tcpThread(void *data);
 void ctrlCHandler(int signal);
+
+int initPWM(PWM *pwm);
+void setPWM(float PWMval[], float pwm1, float pwm2, float pwm3, float pwm4);
+void setAllPWM(float PWMval[], float val);
+void sendPWM(PWM* pwm, float PWMval[]);
 
 #endif /* NAVIO_THREADS_HPP */
